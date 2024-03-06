d1314c151d8319aa8f46e56d46fc276e1b0ba61c
if095920 <162432077+if095920@users.noreply.github.com>
2024-03-06 16:49:32 +0700

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -3,10 +3,16 @@ class Calculator
 	static void Main(string[] args)
 	{
 		
+		
 	}
 
 	static int Penambahan(int a, int b)
 	{
 		return a + b;
+	}
+
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
